34d22bac6c56cacc85bbe3086ebe3ae6f45576d8
izayoiriko <mmoursnuimneg@gmail.com>
2020-03-05 07:21:33 +0000

    3/5 D6 paginate 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200304115859) do
+ActiveRecord::Schema.define(version: 20200305062146) do
 
   create_table "attendances", force: :cascade do |t|
     t.string "worked_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20200304115859) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
