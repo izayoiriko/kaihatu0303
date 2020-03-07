99b41642c28f5a143b06ef11971f918695a6a1c2
izayoiriko <mmoursnuimneg@gmail.com>
2020-03-07 00:26:03 +0000

    3/7 task_model 存在性修正 追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ApplicationRecord
   belongs_to :user
   
   validates :work, presence: true, length: { maximum: 20 }
-  validates :details, presence: true, length: { maxmum: 100 }
+  validates :details, presence: true, length: { maximum: 100 }
   
 end
