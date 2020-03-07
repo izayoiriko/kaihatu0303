c0628a10bd395ed1b59d6da3102586f3499ce3c1
izayoiriko <mmoursnuimneg@gmail.com>
2020-03-07 01:37:04 +0000

    3/7 フレンドリーフォワーディング 追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
-  def current_user?(user)
-    user == current_user
-  end
 end
