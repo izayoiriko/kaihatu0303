fe02670766e8e7ab4d61bcfbc802dc5cfa6db037
izayoiriko <mmoursnuimneg@gmail.com>
2020-03-07 02:45:28 +0000

    3/7 paginate 日本語化 追加

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# ページネーションの設定
+WillPaginate::ViewHelpers.pagination_options[:previous_lavel] = '前ページ'
+WillPaginate::ViewHelpers.pagination_options[:next_lavel] = '次ページ'
