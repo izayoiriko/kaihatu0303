ca44ea023619c15aaa6b9c48770ee5c122ccfe35
izayoiriko <mmoursnuimneg@gmail.com>
2020-03-09 09:02:14 +0000

    3/9 application_helper text_url_to_link(details) 追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
       page_name + " | " + base_title
     end
   end
+  
+  require "uri"
+  
+  def text_url_to_link(details)
+    
+    URI.extract(details, ["http", "https"]).uniq.each do |url|
+      sub_details = ""
+      sub_details << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      
+      details.gsub!(url, sub_details)
+    end
+    return details
+  end
 end
